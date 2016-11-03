25472a6652fa5ffa00bd2753143989d9ff5c91f6
Joshtrich <Joshtrich@wireless-10-146-47-213.public.utexas.edu>
2016-11-03 16:33:33 -0500

    Set up grid display for world

--- src/assignment5/Main.java
+++ src/assignment5/Main.java
@@ -7,6 +7,9 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
@@ -16,6 +19,8 @@ import javafx.stage.Stage;
 public class Main extends Application{
 
 	static GridPane grid = new GridPane();
+	static GridPane controller = new GridPane();
+	Stage critterDisplay = new Stage();
 	
 	public static void main(String[] args) {
 		launch(args);
@@ -24,6 +29,7 @@ public class Main extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.setTitle("Critter Controller");
+		critterDisplay.setTitle("Critter World");
 		grid.setGridLinesVisible(true);
 		
 		FlowPane flow = new FlowPane();
@@ -34,10 +40,32 @@ public class Main extends Application{
 	    Circle c = new Circle(50);
 	    c.setFill(javafx.scene.paint.Color.RED);
 	    flow.getChildren().add(c);
-	    grid.add(flow, 0, 0);
+	    controller.add(flow, 110, 0);
 	    
-		primaryStage.setScene(new Scene(grid, 1200, 500));
+		primaryStage.setScene(new Scene(controller, 500, 500));
+		createWorld(grid);
+		critterDisplay.setScene(new Scene(grid));
+		critterDisplay.show();
 		primaryStage.show();
+	}
+	
+	/**
+	 * Create the visual grid for the critter world using params given
+	 * @param world
+	 */
+	public static void createWorld(GridPane world){
+		for (int i = 0; i < Params.world_height; i++){
+			for (int j = 0; j < Params.world_width; j++){
+				StackPane spot = new StackPane();
+				spot.setBackground(new Background(new BackgroundFill(javafx.scene.paint.Color.BLACK, CornerRadii.EMPTY, null)));
+				Rectangle s = new Rectangle(7.5, 7.5);
+				s.setStroke(javafx.scene.paint.Color.BLACK);
+				s.setStrokeWidth(0.5);
+				s.setFill(javafx.scene.paint.Color.WHITE);
+				spot.getChildren().add(s);
+				world.add(spot, j, i);
+			}
+		}
 	}
 
 }
